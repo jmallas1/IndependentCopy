d5be69f8b54f363f07c3775bf60669ceacca9e13
Jared R. Mallas <jrtytn@gmail.com>
2018-10-24 19:35:11 -0500

    stub out base functions functional

--- src/independentcopy/ListReader.java
+++ src/independentcopy/ListReader.java
@@ -2,6 +2,11 @@ package independentcopy;
 
 import java.util.ArrayList;
 
+/**
+ * Class model for an array list reader
+ * @author Jared Mallas
+ * @version 1.0
+ */
 public class ListReader implements Reader
 {
     ArrayList<String> someList;
@@ -11,6 +16,10 @@ public class ListReader implements Reader
         this.someList = someList;
     }
 
+    /**
+     * Return a string of the last element of the array list
+     * @return String representation of the last element of the array list
+     */
     @Override
     public String readln() {
         return someList.get(someList.size() - 1);
--- src/independentcopy/ListWriter.java
+++ src/independentcopy/ListWriter.java
@@ -2,6 +2,11 @@ package independentcopy;
 
 import java.util.ArrayList;
 
+/**
+ * Class model for an array list writer
+ * @author Jared Mallas
+ * @version 1.0
+ */
 public class ListWriter implements Writer
 {
     ArrayList<String> someList;
@@ -11,9 +16,17 @@ public class ListWriter implements Writer
         this.someList = someList;
     }
 
+    /**
+     * Adds a string to the array list
+     * @param line String to add to array list
+     */
     @Override
     public void writeln(String line)
     {
         someList.add(line);
+    }
+
+    public ArrayList<String> getSomeList() {
+        return someList;
     }
 }
